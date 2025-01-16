766d267d2ebb76acad8f233acb6437ed468886b7
Rh4096 <rhelvetican@gmail.com>
2025-01-16 12:08:24 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::{
+    collections::HashSet,
+    error::Error,
+    fs::File,
+    io::{BufWriter, Write},
+    sync::LazyLock,
+};
+
+use rand::{seq::SliceRandom, thread_rng};
+use regex::Regex;
+use ureq::Agent;
+
+static SOURCES: &[&str] = &[
+    "https://t.me/s/warpplus",
+    "https://t.me/s/warppluscn",
+    "https://t.me/s/warpPlusHome",
+    "https://t.me/s/warp_veyke",
+    "https://t.me/s/warp_key",
+];
+
+static PATTERN: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"<code>([A-Za-z0-9-]+)<\/code>").unwrap());
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut rng = thread_rng();
+    let agent = Agent::new();
+    let mut keys = HashSet::new();
+
+    for &url in SOURCES {
+        println!("Currently searching in {}...", url);
+        let body = agent.get(url).call()?.into_string()?;
+
+        for regex_match in PATTERN.find_iter(&body) {
+            let key = regex_match.as_str();
+            let keylen = key.len();
+            keys.insert(String::from(&key[6..(keylen - 7)]));
+        }
+    }
+
+    let mut full = BufWriter::new(File::create("./full.txt")?);
+    let mut lite = BufWriter::new(File::create("./lite.txt")?);
+
+    let keys = keys.into_iter().collect::<Box<[String]>>();
+    let mut written = HashSet::new();
+    if keys.len() > 0 {
+        let mut cnt = 0u8;
+        while cnt < 200 {
+            if let Some(key) = keys.choose(&mut rng) {
+                if written.insert(key) {
+                    writeln!(&mut full, "{}", key)?;
+                }
+            }
+            cnt += 1;
+        }
+
+        cnt = 0;
+        while cnt < 50 {
+            if let Some(key) = keys.choose(&mut rng) {
+                if written.insert(key) {
+                    writeln!(&mut lite, "{}", key)?;
+                }
+            }
+            cnt += 1;
+        }
+    }
+
+    Ok(())
+}
